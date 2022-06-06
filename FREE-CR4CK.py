f8887cfc7eb413b54d185cae8b153f319bf85abf
HafizdXD <101934492+HAFIZDXD@users.noreply.github.com>
2022-06-06 15:00:42 +0700

    Update FREE-CR4CK.py

--- FREE-CR4CK.py
+++ FREE-CR4CK.py
@@ -6,5 +6,4 @@
 Y = '\033[93;1m'
 
 
-class Main:	def __init__(self):		self.id = []		self.ok = []		self.cp = []		self.loop = 0		os.system("clear")		print(logo)
 print('\033[93;1mScript MOKAD AJG \033[0m')
